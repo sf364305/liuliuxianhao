6851d70167c0a9797bc031adfa739d7382befbd1
jijuyuan <404493850@qq.com>
2017-07-05 15:48:11 +0800

    首页接口调通

--- pt/src/components/home.vue
+++ pt/src/components/home.vue
@@ -4,7 +4,7 @@
             <router-link to="/commodity" class="search-index" replace></router-link>
         </header>
         <div>
-           <app-banner :listImg="listImg"></app-banner>
+            <app-banner :listImg="listImg"></app-banner>
         </div>
         <div class="announcement">
             <ul class="announ">
@@ -15,22 +15,27 @@
         </div>
         <section class="new-content">
             <ul class="clearfix">
-               <li><router-link to="/my_sell" class="index-sell-buy" replace>买卖账号</router-link></li>
-               <li><router-link to="/lease" class="index-lease" replace>租赁账号</router-link></li>
-               <li><router-link to="/popularity" class="index-popularity" replace>提高人气</router-link></li>
+                <li>
+                    <router-link to="/my_sell" class="index-sell-buy" replace>买卖账号</router-link>
+                </li>
+                <li>
+                    <router-link to="/lease" class="index-lease" replace>租赁账号</router-link>
+                </li>
+                <li>
+                    <router-link to="/popularity" class="index-popularity" replace>提高人气</router-link>
+                </li>
             </ul>
         </section>
         <ul class="project clearfix">
-           <li><router-link to="/commodity" class="index-ying" replace><img src="../assets/images/ying.png" alt="">映客账号</router-link></li>
-           <li><router-link to="/commodity" class="index-hua" replace><img src="../assets/images/hua.png" alt="">花椒账号</router-link></li>
-           <li><router-link to="/commodity" class="index-mo" replace><img src="../assets/images/mo.png" alt="">陌陌账号</router-link></li>
-           <li><router-link to="/commodity" class="index-hand" replace><img src="../assets/images/hand.png" alt="">快手账号</router-link></li>
-           <li><router-link to="/commodity" class="index-else" replace><img src="../assets/images/esle.png" alt="">其它账号</router-link></li>
+            <li v-for="c in categroy" v-bind:key="c.id">
+                <router-link to="/commodity" class="index-ying" replace>
+                    <img :src="c.img" alt="">{{c.name}}</router-link>
+            </li>
         </ul>
         <div class="index-news">
             <h2>最新寄售信息</h2>
             <ul class="com-list">
-                <li>     
+                <li>
                     <router-link to="/detail" class="game-name" replace>
                         <span class="name-title clearfix">
                             <img src="../assets/images/little2.png" alt="">
@@ -55,7 +60,7 @@
                     </router-link>
                     <div class="sell-status1"></div>
                 </li>
-                <li>     
+                <li>
                     <router-link to="/detail" class="game-lease" replace>
                         <span class="name-title clearfix">
                             <img src="../assets/images/little1.png" alt="">
@@ -82,159 +87,176 @@
     </div>
 </template>
 <style>
-    .index-logo{
-        position: relative;
-        width: 100%;
-        height: 5rem;
-        background: url("../assets/images/logo_index.png") center center no-repeat;
-        background-size: 28.4% auto;
-    }
-    .search-index {
-        position: absolute;
-        right: 6%;
-        top: 0rem;
-        width: 2.2rem;
-        height: 5rem;
-        background: url("../assets/images/search.png") center center no-repeat;
-        background-size: 100% auto;
-    }
-    .announcement {
-        overflow: hidden;
-        width: 85%;
-        height: 3rem;
-        padding-left: 10%;
-        color: #333;
-        background: url("../assets/images/advert.png") 20px center no-repeat;
-        background-size: 4% auto;
-    }
-    .announcement-alert {
-        position: fixed;
-        display: none;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background: rgba(0,0,0,0.8);
-        z-index: 100;
-    }
-    .announ-alert {
-        overflow-x: hidden;
-        width: 92%;
-        height: 20rem;
-        padding-top: 8rem;
-        margin: 0 auto;
-        color: #fff;
-    }
-    .announ-close {
-        position: absolute;
-        top: auto;
-        left: 0;
-        right: 0;
-        bottom: 5rem;
-        margin: auto;
-        width: 2rem;
-        height: 2rem;
-        -webkit-border-radius: 50%;
-        -moz-border-radius: 50%;
-        border-radius: 50%;
-        border: 1px solid #dcdcdc;
-        color: #fff;
-        line-height: 2rem;
-        text-align: center;
-    }
-    .announ-alert li {
-        margin: 0.5rem 0;
-        
-    }
-    .announ li {
-        overflow: hidden;
-        height: 3rem;
-        padding-left: 2%;
-        background-color: #fff;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        line-height: 3rem;
-    }
-    .new-content {
-        padding: 0.63rem;
-        background: #e9e9e9;
-    }
-    .new-content ul li {
-        float: left;
-        width: 33%;
-        height: 3rem;
-        border-right: 1px solid #999;
-        line-height: 3rem;
-    }
-    .new-content ul li:last-child {
-        border-right: none;
-    }
-    .new-content ul li a {
-        display: block;
-        padding-left: 45%;
-        font-size: 1.08rem;
-    }
-    .index-sell-buy {
-        background: url("../assets/images/sell_buy.png") 15% 0 no-repeat;
-        background-size: 31% auto;
-    }
-    .index-lease {
-        background: url("../assets/images/lease.png") 15% 0 no-repeat;
-        background-size: 31% auto;
-    }
-    .new-content .index-popularity {
-        background: url("../assets/images/papulity.png") 15% 0 no-repeat;
-        background-size: 31% auto;
-    }
+.index-logo {
+    position: relative;
+    width: 100%;
+    height: 5rem;
+    background: url("../assets/images/logo_index.png") center center no-repeat;
+    background-size: 28.4% auto;
+}
+
+.search-index {
+    position: absolute;
+    right: 6%;
+    top: 0rem;
+    width: 2.2rem;
+    height: 5rem;
+    background: url("../assets/images/search.png") center center no-repeat;
+    background-size: 100% auto;
+}
+
+.announcement {
+    overflow: hidden;
+    width: 85%;
+    height: 3rem;
+    padding-left: 10%;
+    color: #333;
+    background: url("../assets/images/advert.png") 20px center no-repeat;
+    background-size: 4% auto;
+}
+
+.announcement-alert {
+    position: fixed;
+    display: none;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.8);
+    z-index: 100;
+}
+
+.announ-alert {
+    overflow-x: hidden;
+    width: 92%;
+    height: 20rem;
+    padding-top: 8rem;
+    margin: 0 auto;
+    color: #fff;
+}
+
+.announ-close {
+    position: absolute;
+    top: auto;
+    left: 0;
+    right: 0;
+    bottom: 5rem;
+    margin: auto;
+    width: 2rem;
+    height: 2rem;
+    -webkit-border-radius: 50%;
+    -moz-border-radius: 50%;
+    border-radius: 50%;
+    border: 1px solid #dcdcdc;
+    color: #fff;
+    line-height: 2rem;
+    text-align: center;
+}
+
+.announ-alert li {
+    margin: 0.5rem 0;
+}
+
+.announ li {
+    overflow: hidden;
+    height: 3rem;
+    padding-left: 2%;
+    background-color: #fff;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    line-height: 3rem;
+}
+
+.new-content {
+    padding: 0.63rem;
+    background: #e9e9e9;
+}
+
+.new-content ul li {
+    float: left;
+    width: 33%;
+    height: 3rem;
+    border-right: 1px solid #999;
+    line-height: 3rem;
+}
+
+.new-content ul li:last-child {
+    border-right: none;
+}
+
+.new-content ul li a {
+    display: block;
+    padding-left: 45%;
+    font-size: 1.08rem;
+}
+
+.index-sell-buy {
+    background: url("../assets/images/sell_buy.png") 15% 0 no-repeat;
+    background-size: 31% auto;
+}
+
+.index-lease {
+    background: url("../assets/images/lease.png") 15% 0 no-repeat;
+    background-size: 31% auto;
+}
+
+.new-content .index-popularity {
+    background: url("../assets/images/papulity.png") 15% 0 no-repeat;
+    background-size: 31% auto;
+}
 </style>
 
 <script>
-     import Vue from 'vue'
-     //引入组件和图片
-     import Banner from '../templates/Banner.vue'
-     import a from '../assets/images/banner1.png'
-     import b from '../assets/images/banner2.png'
-     var vm = new Vue({ 
-        beforeCreate(){
-            
+import Vue from 'vue'
+//引入组件和图片
+import Banner from '../templates/Banner.vue'
+import a from '../assets/images/banner1.png'
+import b from '../assets/images/banner2.png'
+export default {
+    name: 'home',
+    data() {
+        return {
+            listImg: [{
+                url: a
+            }, {
+                url: b
+            }],
+            notices: [],
+            categroy:[]
+        }
+    },
+    created() {
+        this.getToken();
+        this.getNotices();
+        this.getCategroy();
+    },
+    methods: {
+        getToken() {
+            this.Http.setToken(this.GetQueryString('token'));
         },
-        created(){
-            var that = this;
-            this.Http.get(this.Api.getToken(),null,function(result){
-                // console.log(result);
-                that.Http.setToken(result.data.token);
-                // debugger;
-                that.Http.get(that.Api.getQiniuToken(),null,function(_result){
-                    // console.log(_result);
-                });
-            });
-
+        GetQueryString(name) {
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+            var r = window.location.search.substr(1).match(reg);
+            if (r != null) return unescape(r[2]); return null;
+        },
+        getNotices() {
             //获取公告
-            this.Http.get(this.Api.getNotices(),null,function(result){
-                // debugger;
-                // that.$set('notices',result.data.notices);
-                // that.notices = (result.data.notices);
-                that.$set(that.notices,result.data.notices,true) ;
-                // that.msg = '77777';
-                // that.$set('msg','sdfsf');
+            var that = this;
+            this.Http.get(this.Api.getNotices(), {
+                t: "t"
+            }, function (result) {
+                that.notices = (result.data.notices);
+            })
+        },
+        getCategroy(){
+            var that = this;
+            this.Http.get(this.Api.getCategroy(),null, function (result) {
+                that.categroy = result.data.categories;
             })
         }
-    })
-     export default {
-        name: 'home',
-        data() {
-            return {
-                listImg: [{
-                    url: a
-                }, {
-                    url: b
-                }],
-                msg: '6666',
-                notices:[]
-            }
-        },
-        components: {
-            'app-banner': Banner
-        }
+    },
+    components: {
+        'app-banner': Banner
     }
+}
 </script>
--- pt/src/http/Http.js
+++ pt/src/http/Http.js
@@ -7,22 +7,25 @@ const base = 'http://c.chovans.cn/front';
 
 //API,接口列表
 Vue.prototype.Api = {
-    getToken(){
+    getToken() {
         return base + '/utils/getToken';
     },
-    getQiniuToken(){
+    getQiniuToken() {
         return base + '/utils/getUploadToken';
-    } ,
-    getNotices(){
-        return base+'/common/getNotices';
+    },
+    getNotices() {
+        return base + '/common/getNotices';
+    },
+    getCategroy(){
+        return base + '/home/getCategory';
     }
 };
 
 //Http基本类，统一处理
 Vue.prototype.Http = {
-    token:null,
-    setToken(_token){
-        console.log("设置token："+_token);
+    token: null,
+    setToken(_token) {
+        console.log("设置token：" + _token);
         this.token = _token;
     },
     get: function (api, params, callback) {
@@ -33,16 +36,21 @@ Vue.prototype.Http = {
         }
 
         //添加请求token
-        if(params === null){
+        if (params === null) {
             params = {};
         }
         // debugger;
-        if(api !== Vue.prototype.Api.getToken()){
+        if (api !== Vue.prototype.Api.getToken()) {
             params.token = this.token;
         }
-        console.log("参数："+JSON.stringify(params));
+        console.log("参数：" + JSON.stringify(params));
 
-        axios.post(api, params,{emulateJSON: true}).then((response) => {
+        axios.post(api, params, {
+            emulateJSON: true,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }).then((response) => {
             // 响应成功回调
             callback(response.data);
         }, (response) => {
