b5450ac691ad36859cfcd816651c7a18353a6148
jijuyuan <404493850@qq.com>
2017-07-19 16:24:36 +0800

    调试

--- pt/src/components/server.vue
+++ pt/src/components/server.vue
@@ -64,8 +64,8 @@ export default {
 
         //event 参数中有 data 属性，就是父窗口发送过来的数据
         window.addEventListener("message", function (event) {
-            console.log("收到客服系统反馈：",event.data);
-            self.serverOnline = (event.data == "online" ? true:false);
+            console.log("收到客服系统反馈：",event);
+            self.serverOnline = true;
         }, false);
     },
     methods: {
