60f37d0f468694f8f8e51fff78502a132b479ecf
石凡 <1839085395@qq.com>
2017-08-25 11:19:50 +0800

    1

--- pt/src/templates/upload.vue
+++ pt/src/templates/upload.vue
@@ -22,7 +22,10 @@ export default {
     },
     props: ['images'],
     activated() {
-        this.Wx.register();
+        setTimeout(function() {
+            this.Wx.register();
+            this.update();
+        },300)
     },
     methods: {
         wxUpdate() {
@@ -32,6 +35,7 @@ export default {
                 sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
                 sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
                 success: function (res) {
+                    alert('成功')
                     self.localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
                     self.syncUpload(self.localIds);
 
@@ -41,28 +45,31 @@ export default {
         syncUpload(localImagesIds) {
             var self = this;
             var localId = localImagesIds[0];
+            alert('localId')
             //解决IOS无法上传的坑
             if (localId.indexOf("wxlocalresource") != -1) {
                 localId = localId.replace("wxlocalresource", "wxLocalResource");
             }
-            wx.uploadImage({
-                localId: localId,
-                isShowProgressTips: 1,
-                success: function (res) {
-                    var serverId = res.serverId; // 返回图片的服务器端ID
-                    self.Http.get(self.Api.getQiniuImage(), {
-                        mediaId: serverId
-                    }, function (result) {
-                        self.images.push(result.data.key);
-                        //其他对serverId做处理的代码
-                        self.localIds.splice(0, 1);
-                        if (self.localIds.length > 0) {
-                            self.syncUpload(self.localIds);
-                        }
-                    });
+            wx.ready(function() {           
+                wx.uploadImage({
+                    localId: localId,
+                    isShowProgressTips: 1,
+                    success: function (res) {
+                        var serverId = res.serverId; // 返回图片的服务器端ID
+                        self.Http.get(self.Api.getQiniuImage(), {
+                            mediaId: serverId
+                        }, function (result) {
+                            self.images.push(result.data.key);
+                            //其他对serverId做处理的代码
+                            self.localIds.splice(0, 1);
+                            if (self.localIds.length > 0) {
+                                self.syncUpload(self.localIds);
+                            }
+                        });
 
-                }
-            });
+                    }
+                });
+            }
         },
         del(index) {
             var self = this;
