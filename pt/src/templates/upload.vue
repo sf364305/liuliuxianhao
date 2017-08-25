d43420871f8fe36341d5806fa4a34b9c69615a8f
石凡 <1839085395@qq.com>
2017-08-25 11:55:49 +0800

    1

--- pt/src/templates/upload.vue
+++ pt/src/templates/upload.vue
@@ -25,7 +25,7 @@ export default {
         var self = this;
         setTimeout(function() {
             self.Wx.register();
-            // self.update();
+            self.update(e);
         },300)
     },
     methods: {
