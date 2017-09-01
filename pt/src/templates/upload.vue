894edd10837a999eff5e8aaa2e7e1c45c0ce58cb
Chovans <404493850@qq.com>
2017-09-01 17:26:51 +0800

    t

--- pt/src/templates/upload.vue
+++ pt/src/templates/upload.vue
@@ -6,7 +6,7 @@
         </div>
 
         
-        <div v-show="reload" class="realease_picbtn" data="0" @click.native="selectImg()">
+        <div v-show="reload" class="realease_picbtn" data="0" @click="selectImg()">
             <img data-id="img_0" src="../assets/images/add.png" alt="" title="">
             <div style="display:none;" id="none"></div>
             <input id="platFileBtn" name="file" type="button" />
