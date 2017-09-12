c38c426fa55f964412002671c197d294856157f4
石凡 <1839085395@qq.com>
2017-09-12 10:00:51 +0800

    0

--- pt/src/components/sell_infomation.vue
+++ pt/src/components/sell_infomation.vue
@@ -192,7 +192,7 @@
                     </li>
                 </ul>
                 <div class="sell-next">
-                    <input type="button" :disabled="disabled" class="diyStart" @click="addGoods" value="下一步" />
+                    <input type="button" :disabled="disabled" class="diyStart" @click="addGoods" value="下一步了" />
                 </div>
             </form>
         <!-- </scroller> -->
