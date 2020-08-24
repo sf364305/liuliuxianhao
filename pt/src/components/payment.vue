82162edc5528aa082d1cbcdbb8b51cc606f69330
jacky <chenjianyu998@163.com>
2020-08-24 23:33:01 +0800

    添加线下转账

--- pt/src/components/payment.vue
+++ pt/src/components/payment.vue
@@ -3,13 +3,17 @@
     <app-header :header="title"></app-header>
     <div>
       <div class="tab-row">
-        <div class="zhifubao-tab">
+        <div class="zhifubao-tab" :class="{on:tabType == 'zhifubao'}" @click="changePayType('zhifubao')">
           <i><img src="http://qiniu.66xianhao.com/o_1efhilcsh5e81ff717k016t113ck9.jpg"/></i>
           <p>支付宝</p>
         </div>
+        <div class="online-tab" :class="{on:tabType == 'bank'}" @click="changePayType('bank')">
+          <i><img src="http://qiniu.66xianhao.com/o_1egggdr1v111n18fvuq8dc2jnc9.png"/></i>
+          <p>线下转账</p>
+        </div>
 
       </div>
-      <div class="content-div">
+      <div class="content-div" v-if="tabType == 'zhifubao'">
         <div class="info-row">
           <label>支付宝名称：</label>
           <p>六六协创（厦门）网络科技有限公司</p></div>
@@ -18,13 +22,33 @@
           <p>{{account}}</p>
           <span class="copy-btn" v-clipboard:copy="account" v-clipboard:success="onCopy" v-clipboard:error="onError">复制</span>
         </div>
-
-
       </div>
+      <div class="content-div" v-if="tabType == 'bank'">
+        <div class="info-row">
+          <label>开户银行：</label>
+          <p>中国建设银行</p>
+        </div>
+        <div class="info-row">
+          <label>账号户名：</label>
+          <p>{{bankAccountName}}</p>
+          <span class="copy-btn" v-clipboard:copy="bankAccountName" v-clipboard:success="onCopy" v-clipboard:error="onError">复制</span>
+        </div>
+        <div class="info-row">
+          <label>开户行：</label>
+          <p>福建厦门湖滨支行</p>
+        </div>
+        <div class="info-row">
+          <label>银行账号：</label>
+          <p>{{bankAccount}}</p>
+          <span class="copy-btn" v-clipboard:copy="bankAccount" v-clipboard:success="onCopy" v-clipboard:error="onError">复制</span>
+        </div>
+      </div>
+
       <div class="tip-div">
         <div><i class="tip-star">*</i><p class="tip-title">温馨提示：</p></div>
-        <div><p class="tip-content">1、建议先联系客服，确认账号是否还在，再进行转账</p></div>
-        <div><p class="tip-content">2、转账时，请再三确认账号和名称是否正确，如有疑问可以联系官网客服</p></div>
+        <div><p class="tip-content" v-if="tabType == 'zhifubao'">1、建议先联系客服，确认账号是否还在，再进行转账</p></div>
+        <div><p class="tip-content" v-if="tabType == 'zhifubao'">2、转账时，请再三确认账号和名称是否正确，如有疑问可以联系官网客服</p></div>
+        <div><p class="tip-content" v-if="tabType == 'bank'">1、转账充值时，请再三确认银行账号和账号户名是否正确，如有疑问可联系在线客服</p></div>
       </div>
     </div>
   </div>
@@ -33,11 +57,14 @@
     import Header from '../templates/Header.vue'
 
     export default {
-        name: 'certification-else',
+        name: 'payment',
         data() {
             return {
                 title: "平台充值",
-                account: "xmliuliuxiechuang@163.com"
+                account: "xmliuliuxiechuang@163.com",
+                bankAccountName: "六六协创（厦门）网络科技有限公司",
+                bankAccount: "3510 1551 0010 5254 0872",
+                tabType:"zhifubao"
             }
         },
         methods: {
@@ -46,6 +73,9 @@
             },
             onError(e) {
                 this.$iosAlert("复制失败");
+            },
+            changePayType(type){
+                this.tabType = type;
             }
         },
         components: {
@@ -59,30 +89,40 @@
     height: 3rem;
     line-height: 3.5rem;
     margin-left: 1rem;
+    display: flex;
   }
 
-  .zhifubao-tab {
+  .zhifubao-tab,.online-tab{
     padding-left:3rem;
-    border: #0d75d5 solid 1px;
+    border: #a9b1b9 solid 1px;
     border-radius: 0.3rem;
     display: flex;
-    width:12rem;
+    width:10rem;
+    margin-left:1rem;
     box-shadow: 1px 1px 5px 2px rgba(50, 50, 50, 0.2), 1px 1px 1px -11px rgba(50, 50, 50, 0.2);
   }
-  .zhifubao-tab i{
+  .zhifubao-tab i,.online-tab i{
     width: 2rem;
-    padding-top: 0.5rem;
+    padding-top: 0.3rem;
 
     height: 2.5rem;
     margin-right: 0.3rem;
   }
-  .zhifubao-tab img{
+  .zhifubao-tab img,.online-tab img{
     width:100%
   }
 
-  .zhifubao-tab p {
+  .zhifubao-tab p,.online-tab p {
     text-align: center;
-    font-size: 1.6rem;
+    font-size: 1.2rem;
+    line-height: 3rem;
+  }
+  .zhifubao-tab.on{
+    border: 1px solid #07a0f8;
+  }
+
+  .online-tab.on{
+    border: 1px solid #b38f47;
   }
 
   .content-div {
